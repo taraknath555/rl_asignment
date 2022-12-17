9abe24bcb6c04f82ccde06e3f2accd7d021298a6
Tarak <bikashcric1995@gmail.com>
2022-12-17 11:18:46 +0530

    changing folder structure

---
+++ client/src/pages/Books.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import keys from "../config.js/keys";
+import "./books.css";
+import Content from "../components/Content";
+
+const Books = () => {
+  const [books, setBooks] = useState([]);
+  const [noData, setNoData] = useState(false);
+
+  const url = keys.server + "/api/v1/books/show";
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+  const fetchBooks = async (targetFind = "", inputValue = "") => {
+    const finalUrl =
+      inputValue && targetFind ? `${url}?${targetFind}=${inputValue}` : url;
+    const { data } = await axios.get(finalUrl);
+    if (!data.results) setNoData(true);
+    setBooks(data.books);
+  };
+
+  return (
+    <Content
+      data={books}
+      fetchCallback={fetchBooks}
+      dataName="books"
+      noData={noData}
+    />
+  );
+};
+
+export default Books;
