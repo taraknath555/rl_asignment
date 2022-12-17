1103e8ce4e532bdc53bb71bca456c28f147c383c
Tarak <bikashcric1995@gmail.com>
2022-12-17 14:51:37 +0530

    reverting all

--- routes/bookRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const bookController = require("../controllers/bookController");
-const router = express.Router();
-
-router.route("/show").get(bookController.getBooks);
-
-module.exports = router;
--- routes/magazineRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const magazineController = require("../controllers/magazineController");
-const router = express.Router();
-
-router.route("/show").get(magazineController.getMagazines);
-
-module.exports = router;
